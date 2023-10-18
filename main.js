daf23b6b7b161e02418dad9ec4f1b8568de9e9aa
lrjia <lrjia2017@gmail.com>
2023-10-19 01:05:42 +0800

    update

--- main.js
+++ main.js
@@ -15,17 +15,51 @@ function is_post_page(url) {
 }
 
 function post_id(url) {
-    let id = url.split('/t/')[1];
+    // https://www.v2ex.com/t/982998#reply129
+    let id = url.split('/t/')[1].split('#')[0];
     console.log("post id: " + id);
     return id;
 }
 
 function record_post(url) {
+    // #Main > div:nth-child(4) > div:nth-child(1) > span
+    // 129 条回复
+    let reply_count = document.querySelector('#Main > div:nth-child(4) > div:nth-child(1) > span').innerText.split(' ')[0];
+    console.log("reply count: " + reply_count);
 
+    // save
+    let id = post_id(url);
+    let data = {
+        id: id,
+        reply_count: reply_count,
+    }
+    let json = JSON.stringify(data);
+    console.log("json: " + json);
+    GM_log("json: " + json);
+    GM_setValue(id, json);
 }
 
 function edit_index() {
-
+    //div.cell.item
+    let items = document.querySelectorAll('div.cell.item');
+    console.log("items count: " + items.length);
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        let a = item.querySelector('a');
+        let url = a.href;
+        let id = post_id(url);
+        let json = GM_getValue(id);
+        if (json) {
+            let data = JSON.parse(json);
+            let past_reply_count = data.reply_count;
+            let reply_count = a.innerText
+            let new_reply_count = parseInt(reply_count) - parseInt(past_reply_count);
+            a.innerText = reply_count + ' (' + new_reply_count + ')';
+            if (new_reply_count === 0) {
+                a.style.visibility = 'hidden';
+            }
+        }
+    }
 }
 
 
